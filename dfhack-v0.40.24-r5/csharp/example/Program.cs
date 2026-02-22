b7447c4798c460aca9ddf3925952a18d0fa2aea9
mberdev <127224233+mberdev@users.noreply.github.com>
2026-02-22 20:38:04 +0100

    -

--- dfhack-v0.40.24-r5/csharp/example/Program.cs
+++ dfhack-v0.40.24-r5/csharp/example/Program.cs
@@ -8,8 +8,8 @@ try
     var channel = new Channel("localhost", 5000, ChannelCredentials.Insecure);
     var client = new BasicApiRpcService.BasicApiRpcServiceClient(channel);
 
-    var version = client.GetVersion(new EmptyMessage());
-    Console.WriteLine("Succesffully called method 'GetVersion' of DFHack.");
+    var version = client.GetDFVersion(new EmptyMessage());
+    Console.WriteLine("Succesffully called method 'GetDFVersion' of DFHack.");
     Console.WriteLine("DFHack version: " + version.Value);
 
     await channel.ShutdownAsync();
